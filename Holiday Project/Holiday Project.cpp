3a791572edf21a02652e1aa0e68b518c8f849f24
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-01-20 04:38:09 -0600

    Refractored code and moved functions from main file to Item file as helper functions

--- Holiday Project/Holiday Project.cpp
+++ Holiday Project/Holiday Project.cpp
@@ -4,145 +4,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include "Item.h"
 
-//adds a single item
-wrk::Item addItem() {
-	int id = 0;
-	std::string name;
-	float price;
-	std::cout << "\nProduct ID: ";
-	std::cin >> id;
-	std::cout << "\nProduct Name: ";
-	std::cin.ignore(1);
-	std::getline(std::cin, name);
-	std::cout << "\nProduct Price: ";
-	std::cin >> price;
-	//TODO: Check if id,name, price are correct i.e no other item has this id, name is just char, and price is float
-	wrk::Item temp(id, name, price);
-	std::fstream ofs("ItemList.csv", std::ios_base::app);
-	ofs << "\n" << id << ',' << name << ',' <<price;
-	ofs.close();
-	return temp;
-}
 
-//Finds product by ID and update product
-void UpdateProduct(std::vector<wrk::Item>& VecItems) {
-	std::cout << "Enter Product ID >> ";
-	int pid;
-	std::string t_name;
-	float t_price;
-	std::cin >> pid;
-	for (auto& currentProduct : VecItems)
-	{
-		if (currentProduct.getID() == pid)
-		{
-			std::cout << "\nNew Name>> ";
-			std::cin >> t_name;
-			std::cout << "\nNew Price>> ";
-			std::cin >> t_price;
-			currentProduct.updateName(t_name);
-			currentProduct.updatePrice(t_price);
-
-			//updating file
-			std::fstream fs("ItemList.csv");
-			std::string line;
-			long pos = 0;
-			std::getline(fs, line, '\n');//heading
-
-			while (!fs.eof())
-			{
-				std::getline(fs, line, '\n');
-				if (std::stoi(line.substr(0,line.find(','))) == pid) //if line with same product id found
-				{
-					//pos = fs.tellp();
-					//fs.seekp(pos - 5);
-					//fs.
-					//fs.see
-				}
-			}
-		}
-	}
-}
-
-//gets all the items from the .csv file and adds them to the sent array
-void GetItems(std::vector<wrk::Item>& VecItems) {
-	int id = 0;
-	std::string name;
-	float price;
-
-	std::ifstream ifs("ItemList.csv", std::ios::in);
-	if (ifs.is_open() == false)
-		throw "File Could not be opened for reading";
-	std::string line;
-	std::getline(ifs, line, '\n');//Dumbs first line
-
-	while (!ifs.eof())
-	{
-		std::getline(ifs, line, '\n');
-		if (line.length() != 0)
-		{
-		id = std::stoi(line.substr(0, ','));
-		line = line.substr(line.find(',') + 1);
-		name = line.substr(0, line.find(','));
-		line = line.substr(line.find(',') + 1);
-		price = std::stof(line);
-		VecItems.push_back(wrk::Item(id, name, price));
-		}
-	}
-}
-
-void ViewItems(std::vector<wrk::Item>& VecItems){
-	VecItems[0].display(std::cout, true);
-	
-	for (std::vector < wrk::Item>::iterator itr = VecItems.begin()+1; itr != VecItems.end(); itr++)
-	{
-		std::cout << (*itr);
-	}
-}
-
-// finds product by id and deletes it
-void deleteProduct(std::vector<wrk::Item>& VecItems) {
-	std::cout << "Enter Product ID >> ";
-	int pid;
-	std::string t_name;
-	float t_price;
-	std::cin >> pid;
-	for (std::vector<wrk::Item>::iterator itr = VecItems.begin(); itr != VecItems.end(); itr++)
-	{
-		if ((*itr).getID() == pid)
-		{
-			(*itr).display(std::cout, true);
-			
-
-			//line.replace(line.find(deleteline), deleteline.length(), "");
-
-			//updating file
-			std::fstream fs("ItemList.csv");
-			std::string line;
-			long pos = 0;
-			std::getline(fs, line, '\n');//heading
-
-			while (!fs.eof())
-			{
-				std::getline(fs, line, '\n');
-				if (std::stoi(line.substr(0, line.find(','))) == pid) //if line with same product id found
-				{
-					//line.replace(line.find(pid), line.length(), "");
-					//pos = fs.tellp();
-					//fs.seekp(pos - 5);
-					//fs.
-					//fs.see
-				}
-			}
-			VecItems.erase(itr);
-			break;
-		}
-	}
-}
-
-
-int main()
+int main(int argc, char *argv[])
 {
 	std::vector<wrk::Item> VecItems;
 
@@ -157,18 +23,21 @@ int main()
 		switch (option)
 		{
 		case(1):
-			VecItems.push_back(addItem());
+		   VecItems.push_back(wrk::createItem());
+		   wrk::writeOneRow(argv[0],VecItems.back());
 			break;
 		case(2):
 			VecItems.clear();
-			GetItems(VecItems);
-			ViewItems(VecItems);
+			wrk::readFile(argv[0], VecItems);
+
+			//GetItems(VecItems);
+			//ViewItems(VecItems);
 			break;
 		case(3):
-			UpdateProduct(VecItems);
+			//UpdateProduct(VecItems);
 			break;
 		case(4):
-			deleteProduct(VecItems);
+			//deleteProduct(VecItems);
 		case(5):
 			std::cout << "\nGood Bye!";
 			break;
--- Holiday Project/Item.cpp
+++ Holiday Project/Item.cpp
@@ -1,3 +1,7 @@
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 #include "Item.h"
 
 
@@ -82,4 +86,103 @@ std::ostream & wrk::operator<<(std::ostream & os, const Item & i)
 {
 	i.display(os, false);
 	return os;
+}
+
+void wrk::readFile(std::string fsName, std::vector<wrk::Item> VecItems)
+{
+	int id = 0;
+	std::string name;
+	float price;
+
+	std::ifstream ifs(fsName, std::ios::in);
+	if (ifs.is_open() == false)
+		throw "File Could not be opened for reading";
+	std::string line;
+	std::getline(ifs, line, '\n');//Dumbs first line
+
+	while (!ifs.eof())
+	{
+		std::getline(ifs, line, '\n');
+		if (line.length() != 0)
+		{
+			id = std::stoi(line.substr(0, ','));
+			line = line.substr(line.find(',') + 1);
+			name = line.substr(0, line.find(','));
+			line = line.substr(line.find(',') + 1);
+			price = std::stof(line);
+			VecItems.push_back(wrk::Item(id, name, price));
+		}
+	}
+}
+
+//writes all the Item objects to a file in .csv format
+void wrk::writeFile(std::string fsName, std::vector<wrk::Item>& VecItems)
+{
+	std::fstream ofs(fsName, std::ios_base::out);
+	for (Item& vectorItem : VecItems)
+	{
+		ofs << "\n" << vectorItem.getID() << ',' << vectorItem.getName() << ',' << vectorItem.getPrice();
+	}
+	ofs.close();
+}
+
+
+//writes one Item object to a file in .csv format
+void wrk::writeOneRow(std::string fsName,Item & VectorItem)
+{
+	std::fstream ofs(fsName, std::ios_base::app);
+	ofs << "\n" << VectorItem.getID() << ',' << VectorItem.getName() << ',' << VectorItem.getPrice();
+	ofs.close();
+
+}
+
+void wrk::viewItems(std::ofstream & ifs, std::vector<wrk::Item>& VecItems)
+{
+	VecItems[0].display(ifs, true);
+	for (std::vector < wrk::Item>::iterator itr = VecItems.begin() + 1; itr != VecItems.end(); itr++)
+	{
+		ifs << (*itr);
+	}
+}
+
+//sorts vector of item objects based on ID
+void wrk::sortObjects(std::vector<wrk::Item>& vecItems)
+{
+	std::sort(vecItems.begin(), vecItems.end(), [](wrk::Item current, wrk::Item next) {
+		return current.getID() < next.getID();
+	});
+
+}
+
+//deletes a single Item from a vector of products
+void wrk::deleteItem(std::vector<wrk::Item>& VecItems, std::ostream & os, std::istream & is)
+{
+	os << "Enter Product ID >> ";
+	int pid;
+	is >> pid;
+	for (std::vector<wrk::Item>::iterator itr = VecItems.begin(); itr != VecItems.end(); itr++)
+	{
+		if ((*itr).getID() == pid)
+		{
+			(*itr).display(os, true);
+			VecItems.erase(itr);
+			break;
+		}
+	}
+}
+
+wrk::Item wrk::createItem()
+{
+	int id = 0;
+	std::string name;
+	float price;
+	std::cout << "\nProduct ID: ";
+	std::cin >> id;
+	std::cout << "\nProduct Name: ";
+	std::cin.ignore(1);
+	std::getline(std::cin, name);
+	std::cout << "\nProduct Price: ";
+	std::cin >> price;
+	//TODO: Check if id,name, price are correct i.e no other item has this id, name is just char, and price is float
+	return Item(id, name, price);
 }
